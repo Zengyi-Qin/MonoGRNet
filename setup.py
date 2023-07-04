a031126f1f6bf10924c4494d0e42e66fb5d19bd4
Zengyi Qin <qinzy.cs@gmail.com>
2023-07-04 13:27:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ os.system('cd ./include/utils/ && make')
 os.system('cd ./submodules/KittiEvaluation/ && make')
 
 # download the pretrained model
-if not os.path.exists('./data/model_2D.pkl'):
-    os.system('wget https://cloud.tsinghua.edu.cn/f/d958b1da1c49496fa951/?dl=1 -O pretrained.tar.gz')
-    os.system('tar -xf pretrained.tar.gz')
-    os.system('rm pretrained.tar.gz')
+# if not os.path.exists('./data/model_2D.pkl'):
+#     os.system('wget https://cloud.tsinghua.edu.cn/f/d958b1da1c49496fa951/?dl=1 -O pretrained.tar.gz')
+#     os.system('tar -xf pretrained.tar.gz')
+#     os.system('rm pretrained.tar.gz')
 
-print('Pretrained model downloaded.')
+# print('Pretrained model downloaded.')
 
 # check for Kitti data
 if not os.path.exists('./data/KittiBox/training'):
